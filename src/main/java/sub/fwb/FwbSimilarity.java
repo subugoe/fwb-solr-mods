c010fec16653a09f980fd780a229643f86339780
Dennis Neumann <neumann@sub.uni-goettingen.de>
2016-06-20 13:42:25 +0000

    Add Similarity that ignores idf and fieldNorm

---
+++ src/main/java/sub/fwb/FwbSimilarity.java
@@ -0,0 +1,18 @@
+package sub.fwb;
+
+import org.apache.lucene.index.FieldInvertState;
+import org.apache.lucene.search.similarities.DefaultSimilarity;
+
+public class FwbSimilarity extends DefaultSimilarity {
+
+	@Override
+	public float lengthNorm(FieldInvertState state) {
+		return state.getBoost();
+	}
+
+	@Override
+	public float idf(long docFreq, long numDocs) {
+		return (float)1.0;
+	}
+
+}

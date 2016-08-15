4fa89b55fea3b1339f78e842accba33f2ad507b3
Dennis Neumann <neumann@sub.uni-goettingen.de>
2016-08-15 15:23:50 +0000

    Add ability to highlight quote instead of article

---
+++ src/main/java/sub/fwb/HlSnippetAdaptingComponent.java
@@ -0,0 +1,41 @@
+package sub.fwb;
+
+import java.io.IOException;
+
+import org.apache.solr.common.util.SimpleOrderedMap;
+import org.apache.solr.handler.component.ResponseBuilder;
+import org.apache.solr.handler.component.SearchComponent;
+import org.apache.solr.response.SolrQueryResponse;
+
+public class HlSnippetAdaptingComponent extends SearchComponent {
+	
+    @Override
+    public void prepare(ResponseBuilder rb) throws IOException {
+    }
+
+    @Override
+    public void process(ResponseBuilder rb) throws IOException {
+        SolrQueryResponse response = rb.rsp;
+        SimpleOrderedMap<Object> highlightedDocs = (SimpleOrderedMap<Object>) response.getValues().get("highlighting");
+        for (int i = 0; i < highlightedDocs.size(); i++) {
+        	SimpleOrderedMap<Object> currentDoc = (SimpleOrderedMap<Object>) highlightedDocs.getVal(i);
+        	
+        	String[] articleHl = (String[]) currentDoc.get("artikel_text");
+        	if (isEmpty(articleHl)) {
+        		String[] quoteHl = (String[]) currentDoc.get("zitat_text");
+        		currentDoc.add("artikel_text", quoteHl);
+        		currentDoc.remove("zitat_text");
+        	}
+        }
+    }
+
+    private boolean isEmpty(String[] array) {
+    	return array == null || array.length == 0;
+    }
+    
+    @Override
+    public String getDescription() {
+        return "Modifier for highlighted snippets";
+    }
+
+}

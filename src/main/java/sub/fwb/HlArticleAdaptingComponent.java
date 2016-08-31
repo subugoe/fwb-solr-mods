19607cd6516b11e642ffe6e3a782bc274e2b9a00
Dennis Neumann <neumann@sub.uni-goettingen.de>
2016-08-31 13:22:02 +0000

    Highlight quotes (zitat) correctly in article

---
+++ src/main/java/sub/fwb/HlArticleAdaptingComponent.java
@@ -0,0 +1,65 @@
+package sub.fwb;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.solr.common.util.SimpleOrderedMap;
+import org.apache.solr.handler.component.ResponseBuilder;
+import org.apache.solr.handler.component.SearchComponent;
+import org.apache.solr.response.SolrQueryResponse;
+
+public class HlArticleAdaptingComponent extends SearchComponent {
+
+	@Override
+	public void prepare(ResponseBuilder rb) throws IOException {
+	}
+
+	@Override
+	public void process(ResponseBuilder rb) throws IOException {
+		SolrQueryResponse response = rb.rsp;
+		@SuppressWarnings("unchecked")
+		SimpleOrderedMap<Object> highlightedFields = (SimpleOrderedMap<Object>) ((SimpleOrderedMap<Object>) response
+				.getValues().get("highlighting")).getVal(0);
+		String article = ((String[]) highlightedFields.get("artikel"))[0];
+		String[] quotes = (String[]) highlightedFields.get("zitat");
+		if (quotes != null) {
+			for (String quote : quotes) {
+				String quoteStart = getStart(quote);
+				String quoteEnd = getEnd(quote);
+				String quoteMiddle = getMiddle(quote);
+				article = article.replaceFirst("(?s)" + quoteStart + ".*?" + quoteEnd, quoteMiddle);
+			}
+			highlightedFields.add("artikel", new String[] { article });
+
+			highlightedFields.remove("zitat");
+		}
+	}
+
+	private String getStart(String quote) {
+		return extract("(<!--start .*?-->)", quote);
+	}
+
+	private String getEnd(String quote) {
+		return extract("(<!--end .*?-->)", quote);
+	}
+
+	private String getMiddle(String quote) {
+		return extract("<!--start .*?-->(.*)<!--end .*?-->", quote);
+	}
+
+	private String extract(String regex, String s) {
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(s);
+		while (matcher.find()) {
+			return matcher.group(1);
+		}
+		return "";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Modifier for highlighted article";
+	}
+
+}

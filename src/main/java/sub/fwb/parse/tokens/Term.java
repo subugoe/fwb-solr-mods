d15cf5d0df3d909e1434161204fc06f1a0d47aa8
Dennis Neumann <neumann@sub.uni-goettingen.de>
2016-12-22 10:50:52 +0000

    Start refactoring if statements in terms

--- src/main/java/sub/fwb/parse/tokens/Term.java
+++ src/main/java/sub/fwb/parse/tokens/Term.java
@@ -9,16 +9,67 @@ import sub.fwb.parse.ParseUtil;
 
 public class Term extends QueryToken {
 
+	private String articleField;
+	private String citationField;
+	private String articleTextField;
+	private String citationTextField;
+	protected String searchWord;
+
 	public Term(String tokenString, String prefixEnding, Map<String, String> mapForFacetQueries) throws ParseException {
 		this.prefixEnding = prefixEnding;
 		originalTokenString = tokenString;
 		this.mapForFacetQueries = new HashMap<>(mapForFacetQueries);
 		escapeSpecialChars();
 		ParseUtil.checkForProhibitedCharsInTerm(escapedString);
+		articleField = ParseUtil.article(prefixEnding);
+		citationField = ParseUtil.citation(prefixEnding);
+		articleTextField = ParseUtil.articleText(prefixEnding);
+		citationTextField = ParseUtil.citationText(prefixEnding);
+	}
+	
+	private Term() {		
+	}
+	
+	private class Fuzzy extends Term {
+
+		private String fuzzy;
+
+		public Fuzzy(String fuzzyEnding) {
+			this.fuzzy = fuzzyEnding;
+		}
+
+		@Override
+		public String getModifiedQuery() {
+			return String.format("%s%s +(%s:%s%s %s:%s%s) bla", searchWord, fuzzy, articleField, searchWord, fuzzy, citationField, searchWord, fuzzy);			
+		}
+
+		@Override
+		public String getHlQuery() throws ParseException {
+			return String.format("%s:%s%s %s:%s%s ", articleTextField, searchWord, fuzzy, citationTextField, searchWord, fuzzy);
+		}
+
+	}
+	
+	private Term createSpecialTerm() {
+		if (escapedString.endsWith("~1") || escapedString.endsWith("~2")) {
+			searchWord = escapedString.substring(0, escapedString.length() - 2);
+			searchWord = ParseUtil.freeFromCircumflexAndDollar(searchWord);
+			String fuzzyEnding = escapedString.substring(escapedString.length() - 2);
+			return new Fuzzy(fuzzyEnding);
+		} else if (escapedString.startsWith("^") && escapedString.endsWith("$")) {
+			return new Term();
+		} else if (escapedString.startsWith("^")) {
+			return new Term();
+		} else if (escapedString.endsWith("$")) {
+			return new Term();
+		} else {
+			return new Term();
+		}
 	}
 
 	@Override
 	public String getModifiedQuery() {
+//		return createSpecialTerm().getModifiedQuery();
 		String articleField = ParseUtil.article(prefixEnding);
 		String citationField = ParseUtil.citation(prefixEnding);
 		if (escapedString.endsWith("~1") || escapedString.endsWith("~2")) {
